cc8d1164eb7113ebc5c933f1e5aa75d8551b0e81
Daynlight <danielstodulski000@gmail.com>
2023-02-21 16:33:22 +0100

    Login Center

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -1,7 +1,18 @@
 const Login = () => {
     return ( 
         <div>
-            asd
+            <div className="d-flex row col-12 pt-5 justify-content-center">
+                <div class="col-3"></div>
+                <div class="col-6 border rounded border-danger d-flex justify-content-center border-3">
+                    asd
+
+
+
+
+
+                </div>
+                <div class="col-3"></div>
+            </div>
         </div>
      );
 }
