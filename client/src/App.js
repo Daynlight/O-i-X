bbee692cac28498011c9366cc86bd3961972b6bd
Daynlight <danielstodulski000@gmail.com>
2023-02-21 03:06:28 +0100

    Useer Box

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import './css/index.css'
+import { useState } from 'react';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import NavBar from './obj/navbar';
 import Error from './obj/Error';
 import User from './obj/user';
 
 function App() {
+  const [Name,SetName] = useState('Annonim');
+  const [Stars, SetStars] = useState(0);
+  const [Photo,SetPhoto] = useState(require('./img/1.png'));
+  const [Friends,SetFriends] = useState([{id:1, name: 'asd',active: true},{id:2,name:'asdasda',active: false}]);
+  
+
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/">
-            <NavBar></NavBar>
+            <NavBar Name={Name}></NavBar>
             <div class='row col-12'>
-              <div class="col-10">
+              <div class="col-9">
                   ads
               </div>
-              <div className="col-2">
-                <User></User>
+              <div className="col-3">
+                <User Name={Name} Stars={Stars} Photo={Photo} Friends={Friends}></User>
               </div>
 
             </div>
--- client/src/obj/navbar.js
+++ client/src/obj/navbar.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-const NavBar = () => {
-    const [Name,setName] = useState('Anonim');
+const NavBar = ({Name}) => {
 
 
 
