7d8e259338ed83cd54390dcb3c08fd9f342e2dea
Daynlight <danielstodulski000@gmail.com>
2023-02-21 17:04:46 +0100

    Login and Register Form

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -27,6 +27,9 @@ const Login = () => {
                         </form>
                         
                     )}
+
+
+                    
                     {Action==='register' &&
                     (  
                         
