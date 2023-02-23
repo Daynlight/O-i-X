b259847a2dee59ac9285a036a8dee3a0ab075371
Daynlight <danielstodulski000@gmail.com>
2023-02-23 05:25:47 +0100

    Form Style

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -99,18 +99,52 @@ const Login = () => {
 
 
     return ( 
-        <div>
-            <div className="d-flex row col-12 pt-5 justify-content-center">
-                <div class="col-3"></div>
-                <div class="col-6  d-flex justify-content-center border-3">
-                    
-                    {Action==='login' &&
-                    (  
+        <body class="loginBack">
+            
+        
+            <div>
+                <div className="d-flex row col-12 pt-5 justify-content-center">
+                    <div class="col-3"></div>
+                    <div class="col-6  d-flex justify-content-center border-3">
                         
-                        <div class="border rounded border-danger">
-                            <div className="d-flex justify-content-center">
-                                <div class="btn btn-primary me-2"><h1>Login</h1></div>
-                                <div onClick={ () => ChangeAction('register') } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
+                        {Action==='login' &&
+                        (  
+                            
+                            <div class=" colors teminal px-5 pt-5">
+                                <div className="d-flex justify-content-center mb-5">
+                                    <div class="btn btn-primary me-2"><h1>Login</h1></div>
+                                    <div onClick={ () => ChangeAction('register') } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
+                                </div>
+
+                                <div class="">
+                                    <div class="col-6 term">
+                                        <h3>
+                                            <Typical
+                                                wrapper="p"
+                                                loop={1}
+                                                steps={[Text + Z]}
+                                            />
+                                        </h3>
+                                    </div>
+                                    <form class="tt" onSubmit={(e) => UpdateLogin(e,{etap})}>
+                                        <input type="text" class="col-12" value={Z} autoFocus onChange={(e) => {SetZ(e.target.value)}} />
+                                    </form>
+
+                                </div>
+
+                            </div>
+                            
+                        )}
+
+
+
+                        {Action==='register' &&
+                        (  
+                            
+                            <div class="colors teminal px-5 pt-5">
+                            <div className="d-flex justify-content-center mb-5">
+                                <div onClick={ () => ChangeAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
+                                <div  class="btn ms-2 btn-primary "><h1>Register</h1></div>
                             </div>
 
                             <div class="">
@@ -123,54 +157,24 @@ const Login = () => {
                                         />
                                     </h3>
                                 </div>
-                                <form class="tt" onSubmit={(e) => UpdateLogin(e,{etap})}>
+                                <form class="tt" onSubmit={(e) => UpdateRgister(e,{etap})}>
                                     <input type="text" class="col-12" value={Z} autoFocus onChange={(e) => {SetZ(e.target.value)}} />
                                 </form>
 
                             </div>
-
                         </div>
                         
-                    )}
+                        )}
 
 
 
-                    {Action==='register' &&
-                    (  
-                        
-                        <div class="border rounded border-danger">
-                        <div className="d-flex justify-content-center">
-                            <div onClick={ () => ChangeAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
-                            <div  class="btn ms-2 btn-primary "><h1>Register</h1></div>
-                        </div>
 
-                        <div class="">
-                            <div class="col-6 term">
-                                <h3>
-                                    <Typical
-                                        wrapper="p"
-                                        loop={1}
-                                        steps={[Text + Z]}
-                                    />
-                                </h3>
-                            </div>
-                            <form class="tt" onSubmit={(e) => UpdateRgister(e,{etap})}>
-                                <input type="text" class="col-12" value={Z} autoFocus onChange={(e) => {SetZ(e.target.value)}} />
-                            </form>
 
-                        </div>
                     </div>
-                     
-                    )}
-
-
-
-
-
+                    <div class="col-3"></div>
                 </div>
-                <div class="col-3"></div>
             </div>
-        </div>
+        </body>
      );
 }
  
