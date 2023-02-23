f90e8351b0285cd64c3aa7a5e3bc2382e0029f71
Daynlight <danielstodulski000@gmail.com>
2023-02-23 05:09:01 +0100

    Form

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -1,24 +1,100 @@
-import { useState } from "react";
-const Login = () => {
-    const[Action,SetAction] = useState('login');
-    const[Nick,SetNick] = useState('');
-    const[Email,SetEmail] = useState('');
-    const[Password,SetPassword] = useState('');
-    const[Passsword1,SetPasssword1] = useState('');
+import { useEffect, useState } from "react";
+import Typical from 'react-typical'
 
+const Login = () => {
+    var [Action,SetAction] = useState('login');
+    var [Nick,SetNick] = useState('');
+    var [Email,SetEmail] = useState('');
+    var [Password,SetPassword] = useState('');
+    var [Passsword1,SetPasssword1] = useState('');
+    var [Text,SetText] = useState('Your Nick: ')
+    var [Z,SetZ] = useState('')
+    var [etap,SetEtap] = useState(0);
+    
 
     function ChangeAction(action)
     {
+        if(action==='login')SetText('Your Nick: ');
+        if(action==='register')SetText('Give Us Your UserName: ');
+        SetEtap(0);
         SetAction(action);
     }
     
-    function RunAction(e)
+    function RunActionLogin()
     {
-        e.preventDefault();
-
-
+        
 
     }
+    
+    function UpdateLogin(e,etap)
+    {
+        e.preventDefault();
+        
+       
+        if(etap.etap==0)
+        {
+            SetNick(Z);
+            SetText(Text+Z+' Your Password: ');
+            SetZ('');
+            SetEtap(1);
+        }
+
+        if(etap.etap==1)
+        {
+            SetPassword(Z);
+            Z='********';
+            SetText(Text + Z);
+            SetZ('');
+            SetEtap(2);
+        }
+        if(etap.etap==2)RunActionLogin();
+
+        console.log(etap);
+        
+        
+
+    }
+    function UpdateRgister(e,etap)
+    {
+        e.preventDefault();
+        
+       
+        if(etap.etap==0)
+        {
+            SetNick(Z);
+            SetText(Text+Z+' Your Email: ');
+            SetZ('');
+            SetEtap(1);
+        }
+
+        if(etap.etap==1)
+        {
+            SetEmail(Z);
+            SetText(Text + Z +' Password: ');
+            SetZ('');
+            SetEtap(2);
+        }
+        if(etap.etap==2)
+        {
+            SetPassword(Z);
+            SetText(Text + Z +' RePassword: ');
+            SetZ('');
+            SetEtap(3);
+        }
+        if(etap.etap==3)
+        {
+            SetPasssword1(Z);
+            SetText(Text + Z);
+            SetZ('');
+            SetEtap(4);
+        }
+
+        console.log(etap);
+        
+        
+
+    }
+
 
 
 
@@ -26,21 +102,34 @@ const Login = () => {
         <div>
             <div className="d-flex row col-12 pt-5 justify-content-center">
                 <div class="col-3"></div>
-                <div class="col-6 border rounded border-danger d-flex justify-content-center border-3">
+                <div class="col-6  d-flex justify-content-center border-3">
                     
                     {Action==='login' &&
                     (  
                         
-                        <form onSubmit={RunAction}>
+                        <div class="border rounded border-danger">
                             <div className="d-flex justify-content-center">
                                 <div class="btn btn-primary me-2"><h1>Login</h1></div>
                                 <div onClick={ () => ChangeAction('register') } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
                             </div>
-                            <input type="text" class="col-12 my-1 form-login" required value={Nick} placeholder="Nick" onChange={e=> {SetNick(e.target.value)}}/>
-                            <input type="password" class="col-12 my-1 form-login" required value={Password} placeholder="Password" onChange={e=> {SetPassword(e.target.value)}}/>
-                            <input type="password" class="col-12 my-1 form-login" required value={Passsword1} placeholder="RePassword" onChange={e=> {SetPasssword1(e.target.value)}}/>
-                            <input type="submit" class="col-12 btn my-1 btn-info form-login" value="Login" />
-                        </form>
+
+                            <div class="">
+                                <div class="col-6 term">
+                                    <h3>
+                                        <Typical
+                                            wrapper="p"
+                                            loop={1}
+                                            steps={[Text + Z]}
+                                        />
+                                    </h3>
+                                </div>
+                                <form class="tt" onSubmit={(e) => UpdateLogin(e,{etap})}>
+                                    <input type="text" class="col-12" value={Z} autoFocus onChange={(e) => {SetZ(e.target.value)}} />
+                                </form>
+
+                            </div>
+
+                        </div>
                         
                     )}
 
@@ -49,18 +138,29 @@ const Login = () => {
                     {Action==='register' &&
                     (  
                         
-                        <form onSubmit={RunAction}>
-                            <div class="d-flex justify-content-center">
-                                <div onClick={ () => ChangeAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
-                                <div class="btn btn-primary ms-2"><h1>Register</h1></div>
+                        <div class="border rounded border-danger">
+                        <div className="d-flex justify-content-center">
+                            <div onClick={ () => ChangeAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
+                            <div  class="btn ms-2 btn-primary "><h1>Register</h1></div>
+                        </div>
+
+                        <div class="">
+                            <div class="col-6 term">
+                                <h3>
+                                    <Typical
+                                        wrapper="p"
+                                        loop={1}
+                                        steps={[Text + Z]}
+                                    />
+                                </h3>
                             </div>
-                            <input type="text" class="col-12 my-1 form-login" required value={Nick} placeholder="Nick" onChange={e=> {SetNick(e.target.value)}}/>
-                            <input type="email" class="col-12 my-1 form-login" required value={Email} placeholder="Email" onChange={e=> {SetEmail(e.target.value)}}/>
-                            <input type="password" class="col-12 my-1 form-login" required value={Password} placeholder="Password" onChange={e=> {SetPassword(e.target.value)}}/>
-                            <input type="password" class="col-12 my-1 form-login" required value={Passsword1} placeholder="RePassword" onChange={e=> {SetPasssword1(e.target.value)}}/>
-                            <input type="submit" class="col-12 btn my-1 btn-info form-login" value="Login" />
-                        </form>
-                        
+                            <form class="tt" onSubmit={(e) => UpdateRgister(e,{etap})}>
+                                <input type="text" class="col-12" value={Z} autoFocus onChange={(e) => {SetZ(e.target.value)}} />
+                            </form>
+
+                        </div>
+                    </div>
+                     
                     )}
 
 
