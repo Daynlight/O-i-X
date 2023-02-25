fe8064cce94f266f7786316db34030301208eab3
Daynlight <danielstodulski000@gmail.com>
2023-02-25 18:30:15 +0100

    Backend get

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
+var mysql = require('mysql');
 const app = express();
+
+
 
 
 app.set("view engine","ejs");
 
 
+app.get("/Login/:nick/:password",(req,res) =>
+{
+   
 
+   if(req.params.nick!==undefined && req.params.password!==undefined)
+   {
+
+      var con = mysql.createConnection({
+         host: "localhost",
+         user: "root",
+         password: "",
+         database: "O and X"
+      });
+
+      con.connect(function(err) {
+         if (err) throw err;
+         con.query('SELECT id FROM Users where Nick="'+req.params.nick+'" and Password="'+req.params.password+'"', function (err, result, fields) {
+         if (err) throw err;
+            res.json(result)
+         });
+      });
+   }
+      
+})
 
 
 app.get("*",(req,res) =>
@@ -15,4 +41,4 @@ app.get("*",(req,res) =>
 
 
 
-app.listen(81,() => {console.log("Server is runing")});
+app.listen(8080,() => {console.log("Server is runing")});
