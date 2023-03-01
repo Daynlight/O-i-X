9d11b5c2a1036e6e120ab40d69096879224acde8
Daynlight <danielstodulski000@gmail.com>
2023-03-01 21:49:38 +0100

    Login Animation

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect,useRef, useState } from "react";
 import Cookies from "universal-cookie";
 var md5 = require('md5');
 
@@ -7,6 +7,8 @@ const Login = () => {
     const cookies = new Cookies();
     const timexpire = {path: '/', maxAge: 1209600 };
 
+    
+
     const [Action,SetAction] = useState('login');
     const [Nick,SetNick] = useState('');
     const [Email,SetEmail] = useState('');
@@ -14,7 +16,51 @@ const Login = () => {
     const [Password1,SetPassword1] = useState('');
     const [Text,SetText] = useState('');
     const [ShowPassword,SetShowPassword] = useState(false);
-    
+
+
+    const [FirstLoginPassword,SetFirstLoginPassword] = useState(true);
+
+    const PasswordRef = useRef();
+    const [PasswordAnimation,SetPasswordAnimation] = useState('');
+    const ShowPasswordRef = useRef();
+    const [ShowPasswordAnimation,SetShowPasswordAnimation] = useState('');
+    const NickRef = useRef();
+    const [NickAnimation,SetNickAnimation] = useState('');
+    const SubmitRef = useRef();
+    const [SubmitAnimaton,SetSubmitAnimaton] = useState('');
+    const TerminalRef = useRef();
+    const [TerminalAnimation,SetTerminalAnimation] = useState('');
+
+
+
+    const vis = (ref,visa,option) =>
+    {
+
+        const observer = new IntersectionObserver((ent)  => 
+        {
+            const entry = ent[0];
+            if(entry.isIntersecting==true)
+            {
+                visa(option);
+            }
+            else
+            {
+                visa('');
+            }
+        })
+        observer.observe(ref.current);
+        
+    }
+    useEffect(()=>
+    {
+        if(FirstLoginPassword) vis(NickRef,SetNickAnimation,'nickanimationmove'); else SetNickAnimation('nickanimationmove');
+        if(FirstLoginPassword) vis(PasswordRef,SetPasswordAnimation,'passwordanimationmove'); else SetPasswordAnimation('passwordanimationmove');
+        if(FirstLoginPassword) vis(SubmitRef,SetSubmitAnimaton,'loginanimationmove'); else SetSubmitAnimaton('loginanimationmove');
+        if(FirstLoginPassword) vis(ShowPasswordRef,SetShowPasswordAnimation,'changeanimationmove'); else SetShowPasswordAnimation('changeanimationmove');
+        if(FirstLoginPassword) vis(TerminalRef,SetTerminalAnimation,'terminalanimation'); else SetTerminalAnimation('terminalanimation');
+        SetFirstLoginPassword(false);
+    })
+
     function setShow(as)
     {
         SetShowPassword(as);
@@ -59,6 +105,7 @@ const Login = () => {
         {
             SetText('Fill All Forms');
         }
+
     }
 
 
@@ -68,31 +115,32 @@ const Login = () => {
         
             <div>
                 <div className="d-flex row col-12 pt-5 justify-content-center">
-                    <div class="col-3"></div>
-                    <div class="col-6  d-flex justify-content-center border-3">
+                    <div class="col-4"></div>
+                    <div class="col-4  d-flex justify-content-center border-3">
                         
-                        {Action==='login' &&
+                        {Action==='login' && 
                         (  
                             
-                            <div class=" colors teminal px-5 pt-5">
-                                <div className="d-flex justify-content-center mb-5">
+                            <div ref={TerminalRef} class={`colors teminal termianllogin col-12 ${TerminalAnimation}`} >
+                                <div className="d-flex justify-content-center mt-3">
                                     <div class="btn btn-primary me-2"><h1>Login</h1></div>
                                     <div onClick={ () => SetAction('register') } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
                                 </div>
                                     <div class="d-flex justify-content-center fs-2 text-warning ">{Text}</div>
-                                <div class="">
+                                <div class="d-flex justify-content-start">
                                     
                                     <form class="" onSubmit={(e) => RunActionLogin(e)}>
                                         <div class="mb-3">
-                                          <input type="text" class="form-control mt-1 loginform" required value={Nick} onChange={(e)=> SetNick(e.target.value)} placeholder="Nick" />
-                                          <div class="">
-                                            {!ShowPassword &&<input type="password" class="form-control mt-1 loginform" required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
-                                            {ShowPassword &&<input type="text" class="form-control mt-1 loginform" required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
-                                            {!ShowPassword &&<button type="button" onClick={() => setShow(true)} class="btn btn-dark mt-2 col-12" >Show Password</button>}
-                                            {ShowPassword &&<button type="button" onClick={() => setShow(false)} class="btn btn-secondary mt-2 col-12">Hide Password</button>}
+
+                                            <input type="text" ref={NickRef} class={`form-control loginform loginanimation ${NickAnimation}`}  required value={Nick} onChange={(e)=> SetNick(e.target.value)} placeholder="Nick" />
+                                          
+                                            {!ShowPassword &&<input type="password" ref={PasswordRef} class={`form-control loginform loginanimation ${PasswordAnimation}`} required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
+                                            {ShowPassword &&<input type="text" ref={PasswordRef} class={`form-control loginform loginanimation ${PasswordAnimation}`} required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
+                                            {!ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShow(true)} class={`btn btn-dark col-12 loginanimation ${ShowPasswordAnimation}`} >Show Password</button>}
+                                            {ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShow(false)} class={`btn btn-secondary col-12 loginanimation ${ShowPasswordAnimation}`}>Hide Password</button>}
                                             
-                                          </div>
-                                          <input type="submit" class="form-control mt-1 loginform loginsubmit" required value="Login" />
+                                          
+                                          <input type="submit" ref={SubmitRef} class={`form-control loginform loginsubmit loginanimation ${SubmitAnimaton} `}value="Login" />
                                         </div>
                                     </form>
                                 </div>
@@ -103,8 +151,8 @@ const Login = () => {
                         {Action==='register' &&
                         (  
                             
-                            <div class="colors teminal px-5 pt-5">
-                            <div className="d-flex justify-content-center mb-5">
+                            <div class="colors teminal terminalregister col-12">
+                            <div className="d-flex justify-content-center mt-3">
                                 <div onClick={ () => SetAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
                                 <div  class="btn ms-2 btn-primary "><h1>Register</h1></div>
                             </div>
@@ -125,7 +173,7 @@ const Login = () => {
                         )}
                     </div>
                     
-                    <div class="col-3"></div>
+                    <div class="col-4"></div>
                     
                 </div>
             </div>
