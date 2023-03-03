5f2434c5b19ef56131f8bd3daa9f755d3f2a620d
Daynlight <danielstodulski000@gmail.com>
2023-03-04 00:53:32 +0100

    LLOgin.JS Validation

--- client/src/obj/Login.js
+++ client/src/obj/Login.js
@@ -4,22 +4,20 @@ var md5 = require('md5');
 
 const Login = () => {
     const {RegisterURL,LoginURL,ActiveURL} = require('../BackendLinks');
-    const {FetchReq} = require('../Functions/Fetch');
+    const {FetchReq, FetchData} = require('../Functions/Fetch');
 
     const cookies = new Cookies();
-    const timexpire = {path: '/', maxAge: 1209600 };
+    const CookiesTimeExpire = {path: '/', maxAge: 1209600 };
 
-    const [Action,SetAction] = useState('login');
+    const [Action,SetAction] = useState(true);
     const [Nick,SetNick] = useState('');
     const [Email,SetEmail] = useState('');
     const [Password,SetPassword] = useState('');
-    const [Password1,SetPassword1] = useState('');
-    const [Text,SetText] = useState('');
+    const [RePassword,SetRePassword] = useState('');
+    const [InfoText,SetInfoText] = useState('');
     const [ShowPassword,SetShowPassword] = useState(false);
 
-
-    const [FirstLoginPassword,SetFirstLoginPassword] = useState(true);
-
+    const [FirstLogin,SetFirstLogin] = useState(true);
     const PasswordRef = useRef();
     const [PasswordAnimation,SetPasswordAnimation] = useState('');
     const ShowPasswordRef = useRef();
@@ -33,7 +31,6 @@ const Login = () => {
 
     const vis = (ref,visa,option) =>
     {
-
         const observer = new IntersectionObserver((ent)  => 
         {
             const entry = ent[0];
@@ -47,40 +44,21 @@ const Login = () => {
             }
         })
         observer.observe(ref.current);
-        
     }
 
     useEffect(()=>
     {
-        if(FirstLoginPassword) vis(NickRef,SetNickAnimation,'nickanimationmove'); else SetNickAnimation('nickanimationmove');
-        if(FirstLoginPassword) vis(PasswordRef,SetPasswordAnimation,'passwordanimationmove'); else SetPasswordAnimation('passwordanimationmove');
-        if(FirstLoginPassword) vis(SubmitRef,SetSubmitAnimaton,'loginanimationmove'); else SetSubmitAnimaton('loginanimationmove');
-        if(FirstLoginPassword) vis(ShowPasswordRef,SetShowPasswordAnimation,'changeanimationmove'); else SetShowPasswordAnimation('changeanimationmove');
-        if(FirstLoginPassword) vis(TerminalRef,SetTerminalAnimation,'terminalanimation'); else SetTerminalAnimation('terminalanimation');
-        SetFirstLoginPassword(false);
-    },[FirstLoginPassword,NickAnimation,PasswordAnimation,SubmitAnimaton,ShowPasswordAnimation,TerminalAnimation])
+        if(FirstLogin) vis(NickRef,SetNickAnimation,'nickanimationmove'); else SetNickAnimation('nickanimationmove');
+        if(FirstLogin) vis(PasswordRef,SetPasswordAnimation,'passwordanimationmove'); else SetPasswordAnimation('passwordanimationmove');
+        if(FirstLogin) vis(SubmitRef,SetSubmitAnimaton,'loginanimationmove'); else SetSubmitAnimaton('loginanimationmove');
+        if(FirstLogin) vis(ShowPasswordRef,SetShowPasswordAnimation,'changeanimationmove'); else SetShowPasswordAnimation('changeanimationmove');
+        if(FirstLogin) vis(TerminalRef,SetTerminalAnimation,'terminalanimation'); else SetTerminalAnimation('terminalanimation');
+        SetFirstLogin(false);
+    },[FirstLogin,NickAnimation,PasswordAnimation,SubmitAnimaton,ShowPasswordAnimation,TerminalAnimation])
 
-    function setShow(as)
+    function setShowFunction()
     {
-        SetShowPassword(as);
-    }
-
-    function RunActionLogin(e)
-    {  
-        e.preventDefault();
-        fetch(LoginURL,{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({UserNick: Nick, UserPass: md5(Password)})
-          })
-        .then((resp) => resp.json())
-        .then((apidata) =>
-        {
-            cookies.set('UserID', md5(apidata[0].ID),timexpire);
-            cookies.set('UserNick', md5(Nick),timexpire);
-            cookies.set('UserPass', md5(Password),timexpire);
-        })
-        setTimeout(CheckIFLogin,1000);
+        SetShowPassword(!ShowPassword);
     }
 
     function CheckIFLogin()
@@ -92,107 +70,76 @@ const Login = () => {
         }
     }
 
+    function RunActionLogin(e)
+    {  
+        e.preventDefault();
+        FetchData(LoginURL,{UserNick: Nick, UserPass: md5(Password)},(Resoult)=>
+        {
+            cookies.set('UserID', md5(Resoult[0].ID),CookiesTimeExpire);
+            cookies.set('UserNick', md5(Nick),CookiesTimeExpire);
+            cookies.set('UserPass', md5(Password),CookiesTimeExpire);
+        })
+        setTimeout(CheckIFLogin,1000);
+    }
 
-    
     function RunActionRegister(e)
     {
         e.preventDefault();
-        if(Nick!=='' && Password!=='' && Password1!=='' && Email!=='')
+        if(Nick!=='' && Password!=='' && RePassword!=='' && Email!=='')
         {
-            if(Password===Password1)
-            {
-                fetch(RegisterURL,{
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({UserNick: Nick, UserPass: md5(Password), Email: Email})
-                  })
-                .then((resp) => resp.json())
-                .then((apidata) =>
-                {
-                    SetText(apidata[0].err); 
-                })
-            }
-            else
-            {
-                SetText('Passwords are not same');
-            }
+            if(Password===RePassword) FetchData(RegisterURL,{UserNick: Nick, UserPass: md5(Password), Email: Email},(Resault)=>SetInfoText(Resault[0].err))
+            else SetInfoText('Passwords are not same');
         }
-        else
-        {
-            SetText('Fill All Forms');
-        }
+        else SetInfoText('Fill All Forms');
     }
 
-    useEffect(()=>
-    {
-        CheckIFLogin();
-    })
-
-
     return ( 
-        <body class="">
-            
-        
             <div>
                 <div className="d-flex row col-12 pt-5 justify-content-center">
                     <div class="col-4"></div>
                     <div class="col-4  d-flex justify-content-center border-3">
-                        
-                        {Action==='login' && 
-                        (  
+                        {Action && 
                             <div ref={TerminalRef} class={`colors teminal termianllogin col-12 ${TerminalAnimation}`} >
                                 <div className="d-flex justify-content-center mt-3">
-                                    <div class="btn btn-primary me-2"><h1>Login</h1></div>
-                                    <div onClick={ () => SetAction('register') } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
+                                    <div onClick={ () => {SetAction(!Action);SetInfoText('')} } class="btn btn-primary me-2"><h1>Login</h1></div>
+                                    <div onClick={ () => {SetAction(!Action);SetInfoText('')} } class="btn ms-2 notsetloginaction"><h1>Register</h1></div>
                                 </div>
-                                    
                                 <div class="d-flex justify-content-start">
-                                    
                                     <form class="" onSubmit={(e) => RunActionLogin(e)}>
                                         <div class="mb-3">
-
                                             <input type="text" ref={NickRef} class={`form-control loginform loginanimation ${NickAnimation}`}  required value={Nick} onChange={(e)=> SetNick(e.target.value.toLowerCase())} placeholder="Nick" />
-                                          
                                             {!ShowPassword &&<input type="password" ref={PasswordRef} class={`form-control loginform loginanimation ${PasswordAnimation}`} required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
                                             {ShowPassword &&<input type="text" ref={PasswordRef} class={`form-control loginform loginanimation ${PasswordAnimation}`} required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />}
-                                            {!ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShow(true)} class={`btn btn-dark col-12 loginanimation ${ShowPasswordAnimation}`} >Show Password</button>}
-                                            {ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShow(false)} class={`btn btn-secondary col-12 loginanimation ${ShowPasswordAnimation}`}>Hide Password</button>}
-                                            
+                                            {!ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShowFunction()} class={`btn btn-dark col-12 loginanimation ${ShowPasswordAnimation}`} >Show Password</button>}
+                                            {ShowPassword &&<button type="button" ref={ShowPasswordRef} onClick={() => setShowFunction()} class={`btn btn-secondary col-12 loginanimation ${ShowPasswordAnimation}`}>Hide Password</button>}
                                           <input type="submit" ref={SubmitRef} class={`form-control loginform loginsubmit loginanimation ${SubmitAnimaton} `}value="Login" />
                                         </div>
                                     </form>
                                 </div>
                             </div>
-                        )}
-
-                        {Action==='register' &&
-                        (  
+                        }
+                        {!Action && 
                             <div class="colors teminal terminalregister col-12">
                             <div className="d-flex justify-content-center mt-3">
-                                <div onClick={ () => SetAction('login') } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
-                                <div  class="btn ms-2 btn-primary "><h1>Register</h1></div>
+                                <div onClick={ () => {SetAction(!Action);SetInfoText('')} } class="btn me-2 notsetloginaction"><h1>Login</h1></div>
+                                <div onClick={ () => {SetAction(!Action);SetInfoText('')} } class="btn ms-2 btn-primary "><h1>Register</h1></div>
                             </div>
-                               
-                            <div class="">
-                                
-                                <form class="" onSubmit={(e) => RunActionRegister(e)}>
-                                    <div class="mb-3">
-                                        <input type="text" class="form-control mt-1 loginform" required value={Nick} onChange={(e)=> SetNick(e.target.value.toLowerCase())} placeholder="Nick" />
-                                        <input type="password" class="form-control mt-1 loginform" required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />
-                                        <input type="password" class="form-control mt-1 loginform" required value={Password1} onChange={(e)=> SetPassword1(e.target.value)} placeholder="RePassword" />
-                                        <input type="email" class="form-control mt-1 loginform" required value={Email} onChange={(e)=> SetEmail(e.target.value.toLowerCase())} placeholder="Email" />
-                                        <input type="submit" class="form-control mt-1 loginform loginsubmit" value="Register" />
-                                    </div> 
-                                </form>
-                            </div>
+                            <form class="" onSubmit={(e) => RunActionRegister(e)}>
+                                <div class="mb-3">
+                                    <input type="text" class="form-control mt-1 loginform" required value={Nick} onChange={(e)=> SetNick(e.target.value.toLowerCase())} placeholder="Nick" />
+                                    <input type="password" class="form-control mt-1 loginform" required value={Password} onChange={(e)=> SetPassword(e.target.value)} placeholder="Password" />
+                                    <input type="password" class="form-control mt-1 loginform" required value={RePassword} onChange={(e)=> SetRePassword(e.target.value)} placeholder="RePassword" />
+                                    <input type="email" class="form-control mt-1 loginform" required value={Email} onChange={(e)=> SetEmail(e.target.value.toLowerCase())} placeholder="Email" />
+                                    <input type="submit" class="form-control mt-1 loginform loginsubmit" value="Register" />
+                                </div> 
+                            </form>
                         </div>
-                        )}
+                        }
                     </div>
                     <div class="col-4"></div>
                 </div>
-                <div class="d-flex justify-content-center fs-2 text-danger">{Text}</div>
+                <div class="d-flex justify-content-center fs-2 text-danger">{InfoText}</div>
             </div>
-        </body>
      );
 }
  
