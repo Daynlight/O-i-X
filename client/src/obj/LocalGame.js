1d43325e2ce51e0bac01a0b5c65b5f7735c4cfa8
Daynlight <danielstodulski000@gmail.com>
2023-03-04 03:21:56 +0100

    LocalGame Validation

--- client/src/obj/LocalGame.js
+++ client/src/obj/LocalGame.js
@@ -1,124 +1,73 @@
 import { useState } from "react";
 const LocalGame = () => {
-    const [Place,SetPlace] = useState([{id:1, get:''},{id:2, get:''},{id:3, get:''},{id:4, get:''},{id:5, get:''},{id:6, get:''},{id:7, get:''},{id:8, get:''},{id:9, get:''}])
+    const [Place,SetPlace] = useState([{id:1, Pose:''},{id:2, Pose:''},{id:3, Pose:''},{id:4, Pose:''},{id:5, Pose:''},{id:6, Pose:''},{id:7, Pose:''},{id:8, Pose:''},{id:9, Pose:''}])
     const [Move,SetMove] = useState(0);
-    const [Ture,SetTure] = useState(0);
     const [Win, SetWin] = useState('');
 
-    
-    function setPos(id) 
+    function UpdatePlaces(id) 
     {
+        var NewPlace = [...Place];
         if(Move===0)
         {
-            var NewPlace = [...Place];
-            NewPlace[id-1].get = 'x';
-            SetPlace(NewPlace);
+            NewPlace[id-1].Pose = 'x';
             SetMove(1);
-            SetTure(Ture+1);
-            check();
         }
         if(Move===1)
         {
-            NewPlace = [...Place];
-            NewPlace[id-1].get = 'o';
-            SetPlace(NewPlace);
+            NewPlace[id-1].Pose = 'o';
             SetMove(0);
-            check();
+            
         }
+        SetPlace(NewPlace);
+        CheckIfWin();
     }
 
     function Again()
     {
-        SetPlace([{id:1, get:''},{id:2, get:''},{id:3, get:''},{id:4, get:''},{id:5, get:''},{id:6, get:''},{id:7, get:''},{id:8, get:''},{id:9, get:''}]);
-        SetTure(0);
+        SetPlace([{id:1, Pose:''},{id:2, Pose:''},{id:3, Pose:''},{id:4, Pose:''},{id:5, Pose:''},{id:6, Pose:''},{id:7, Pose:''},{id:8, Pose:''},{id:9, Pose:''}]);
         SetWin('');
     }
 
-    function check() 
-       {
-       var us = 'o';
-       if(Place[0].get===us && Place[1].get===us && Place[2].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[3].get===us && Place[4].get===us && Place[5].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[6].get===us && Place[7].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[0].get===us && Place[3].get===us && Place[6].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[1].get===us && Place[4].get===us && Place[7].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[2].get===us && Place[5].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[0].get===us && Place[4].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[2].get===us && Place[4].get===us && Place[6].get===us)
-       {
-        SetWin(us);
-       }
+    function CheckIfWin() 
+    {
+        var us = 'x';
+        if(Place[0].Pose===us && Place[1].Pose===us && Place[2].Pose===us){SetWin(us);}
+        if(Place[3].Pose===us && Place[4].Pose===us && Place[5].Pose===us){SetWin(us);}
+        if(Place[6].Pose===us && Place[7].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[0].Pose===us && Place[3].Pose===us && Place[6].Pose===us){SetWin(us);}
+        if(Place[1].Pose===us && Place[4].Pose===us && Place[7].Pose===us){SetWin(us);}
+        if(Place[2].Pose===us && Place[5].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[0].Pose===us && Place[4].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[2].Pose===us && Place[4].Pose===us && Place[6].Pose===us){SetWin(us);}
+ 
+        us = 'o';
+        if(Place[0].Pose===us && Place[1].Pose===us && Place[2].Pose===us){SetWin(us);}
+        if(Place[3].Pose===us && Place[4].Pose===us && Place[5].Pose===us){SetWin(us);}
+        if(Place[6].Pose===us && Place[7].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[0].Pose===us && Place[3].Pose===us && Place[6].Pose===us){SetWin(us);}
+        if(Place[1].Pose===us && Place[4].Pose===us && Place[7].Pose===us){SetWin(us);}
+        if(Place[2].Pose===us && Place[5].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[0].Pose===us && Place[4].Pose===us && Place[8].Pose===us){SetWin(us);}
+        if(Place[2].Pose===us && Place[4].Pose===us && Place[6].Pose===us){SetWin(us);}
 
-       us = 'x';
-       if(Place[0].get===us && Place[1].get===us && Place[2].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[3].get===us && Place[4].get===us && Place[5].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[6].get===us && Place[7].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[0].get===us && Place[3].get===us && Place[6].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[1].get===us && Place[4].get===us && Place[7].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[2].get===us && Place[5].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[0].get===us && Place[4].get===us && Place[8].get===us)
-       {
-        SetWin(us);
-       }
-       if(Place[2].get===us && Place[4].get===us && Place[6].get===us)
-       {
-        SetWin(us);
-       }
+        var c = 0
+        for(var i=0;i<9;i++) if(Place[i].Pose==='') c++;
+        if(c===0)SetWin("Draw");
     }
-
 
     return ( 
         <div>
-            {(Win!=='' || Ture=== 5) &&
-                <div class="mx-5 d-flex mb-1 darker justify-content-center border rounded-5">
+            {Win!=='' &&
+                <div class="mx-5 d-flex mb-1 DarkerBackground ActualTime justify-content-center border rounded-5">
                     {Win === 'x' && <div class="fs-1">X Win</div>}
                     {Win === 'o' && <div class="fs-1">O Win</div>}
-                    {Ture === 5 && Win==='' && <div class="fs-1">Draw</div>}
-                    {(Ture === 5 || Win!=='') && <div onClick={() => Again()} class="ms-2 btn btn-primary fs-3">Play Again</div>}
+                    {Win==='Draw' && <div class="fs-1">Draw</div>}
+                    <div onClick={() => Again()} class="ms-2 btn btn-primary fs-3">Play Again</div>
                 </div>
             }
-            <div class="mx-5 row border darker rounded-5 p-3">
+            <div class="mx-5 row border DarkerBackground ActualTime rounded-5 p-3">
                 {Place.map(e=>(
-                    (e.get==='x' && <div key={e.id} class="col-4 plcae">
+                    (e.Pose==='x' && <div key={e.id} class="col-4 plcae">
                         
                     <svg fill="#6c757d" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
                         width="100%" height="100%" class="p-5" viewBox="0 0 94.926 94.926">
@@ -130,19 +79,15 @@ const LocalGame = () => {
                             s1.101-0.224,1.498-0.62l5.473-5.476c0.826-0.827,0.826-2.167,0-2.994L55.931,47.463z"/>
                     </g>
                     </svg>
-
                     </div>) ||
-                    (e.get==='o' && <div key={e.id} class="col-4 plcae">
+                    (e.Pose==='o' && <div key={e.id} class="col-4 plcae">
                         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-circle p-5" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         </svg>
                     </div>) || 
-                    ( e.get==='' && Win==='' && <div key={e.id} onClick={() => setPos(e.id) } class="col-4 plcae"></div>) ||
-                    ( e.get==='' && Win!=='' && <div key={e.id} class="col-4 plcae"></div>)
+                    ( e.Pose==='' && Win==='' && <div key={e.id} onClick={() => UpdatePlaces(e.id) } class="col-4 plcae"></div>) ||
+                    ( e.Pose==='' && Win!=='' && <div key={e.id} class="col-4 plcae"></div>)
                 ))}
-
-
-
             </div>
         </div>
      );
