7cb0d03048b0ab16ae10c8219ade48fa64c31e8b
Daynlight <danielstodulski000@gmail.com>
2023-03-11 18:44:18 +0100

    Fetch err

--- client/src/Functions/Fetch.js
+++ client/src/Functions/Fetch.js
@@ -1,35 +1,26 @@
 function FetchReq(url,data)
 {
-  try{
     fetch(url,
         {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data)
         });
-      }
-      catch(err)
-      {
-        console.log("Error:",err)
-      }
 }
 
 function FetchData(url,data,callback)
 {
-    try{
-        var Response = fetch(url,
+    
+    fetch(url,
         {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify(data)
         })
-      }
-      catch(err)
-      {
-        console.log("Error: ",err)
-      }
-  
-    if(Response.ok) Response.json().then((Data)=>{callback(Data);});
+      .then((Response) =>{
+        if(Response.ok) Response.json().then((Data)=>{callback(Data);});
+        else {console.log("Can not Fetch Data")};
+      })
       
 }
 
